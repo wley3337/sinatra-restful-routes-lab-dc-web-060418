12dc763b1f1cc9fbb06736bdcbe0779e04efeec0
wley3337 <wley3337@gmail.com>
2018-06-27 14:59:50 -0400

    sting interpalation needs two quotes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,55 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+
+  # get '/' do
+  #   erb :index
+  # end
+
+  get '/recipes' do
+    @recipes = Recipe.all
     erb :index
+  end
+#<-----New Item create
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.new
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    @recipe.id = Recipe.last.id
+
+    redirect to("/recipes/#{@recipe.id}")
+  end
+#<-------Show page
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+#<------Update process
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.update(name: @recipe.name, ingredients: @recipe.ingredients, cook_time: @recipe.cook_time)
+    redirect to("/recipes/#{@recipe.id}")
+  end
+
+#<--delete from db
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect to('/recipes')
   end
 end
